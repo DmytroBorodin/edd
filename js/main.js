c5640b505ee8b99b202668db189c32155cc30a26
Dmytro Borodin <48290702+DmytroBorodin@users.noreply.github.com>
2020-06-04 15:50:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict'
 
-let mainInput = document.querySelector('.normal__width');
-let mainInputMin = document.querySelector('.width__500');
+let mainInput = document.querySelector('#main__input');
+let mainInputMin = document.querySelector('#width__500');
 let titleBlock = document.querySelector('.title__block');
 let imgBlock = document.querySelector('.img__block');
 let wrap = document.querySelector('.wrap');
@@ -9,20 +9,33 @@ let mainContent = document.querySelector('.main__content');
 let countriesBlock = document.querySelector('.countries__block');
 let tipsBlock = document.querySelector('.tips__block');
 let tipsBlockMini = document.querySelector('.tips__block__500');
+console.log(mainInput.value);
 
-mainInput.addEventListener('click', () => {
-	countriesBlock.classList.toggle('invisible');
-	tipsBlock.classList.toggle('invisible');
+mainInput.addEventListener('focus', () => {
+	countriesBlock.classList.add('invisible');
+	tipsBlock.classList.remove('invisible');
+})
+mainInput.addEventListener('blur', () => {
+	countriesBlock.classList.remove('invisible');
+	tipsBlock.classList.add('invisible');
 })
 
-
-
-if (window.matchMedia("screen and (min-width: 500px)")){
-	mainInputMin.addEventListener('click', () => {
-		titleBlock.classList.toggle('invisible');
-		imgBlock.classList.toggle('invisible');
-		wrap.classList.toggle('max-height');
-		mainContent.classList.toggle('section__mt');
-		tipsBlockMini.classList.toggle('invisible');
-	})
+let onInput = () => {
+	tipsBlock.innerHTML = mainInput.value;
 }
+
+
+mainInputMin.addEventListener('focusin', () => {
+	titleBlock.classList.add('invisible');
+	imgBlock.classList.add('invisible');
+	wrap.classList.add('max-height');
+	mainContent.classList.add('section__mt');
+	tipsBlockMini.classList.remove('invisible');
+});
+mainInputMin.addEventListener('focusout', () => {
+	titleBlock.classList.remove('invisible');
+	imgBlock.classList.remove('invisible');
+	wrap.classList.remove('max-height');
+	mainContent.classList.remove('section__mt');
+	tipsBlockMini.classList.add('invisible');
+})
