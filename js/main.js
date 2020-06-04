187f315ecdacbf3da7501f8163fadb93e395e601
Dmytro Borodin <48290702+DmytroBorodin@users.noreply.github.com>
2020-06-04 15:06:29 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let mainInput = document.querySelector('.normal__width');
+let mainInput = document.querySelector('#main__input');
 let mainInputMin = document.querySelector('.width__500');
 let titleBlock = document.querySelector('.title__block');
 let imgBlock = document.querySelector('.img__block');
@@ -9,12 +9,16 @@ let mainContent = document.querySelector('.main__content');
 let countriesBlock = document.querySelector('.countries__block');
 let tipsBlock = document.querySelector('.tips__block');
 let tipsBlockMini = document.querySelector('.tips__block__500');
+console.log(mainInput.value);
 
 mainInput.addEventListener('click', () => {
 	countriesBlock.classList.toggle('invisible');
 	tipsBlock.classList.toggle('invisible');
 })
 
+let onInput = () => {
+	tipsBlock.innerHTML = mainInput.value;
+}
 
 
 if (window.matchMedia("screen and (min-width: 500px)")){
